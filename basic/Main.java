5ca5b89b8f30e45a20a6c3d9d16f8531a1aef769
Herickjf <125290993+Herickjf@users.noreply.github.com>
2023-12-08 15:08:33 -0300

    Update and rename Main.java to basic/Main.java

--- basic/Main.java
+++ basic/Main.java
@@ -1,6 +1,5 @@
 //Just doing a canonical code that everyone does in every programming language they learn :)
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         String nome = "Herick";
